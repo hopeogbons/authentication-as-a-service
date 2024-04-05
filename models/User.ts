a835f93e8b52a7bd583d07f30b16ed374bf15390
Hope Ogbons <hopeogbons@gmail.com>
2024-04-05 21:29:32 +0100

    Create a user model

--- models/User.ts
+++ models/User.ts
@@ -1,18 +1,34 @@
 import mongoose from "mongoose"
 import bcrypt from "bcryptjs"
 
-interface UserInterface extends mongoose.Document {
+export interface IUser {
+  id: string
+  email: string
+}
+
+export interface IUserCredentials {
+  email: string
+  password: string
+}
+
+export interface IAuthenticationData {
+  id: string
+  token: string
+}
+
+export interface IUserSchema extends mongoose.Document {
+  id: string
   email: string
   password: string
   comparePassword: (candidatePassword: string) => Promise<boolean>
 }
 
-const UserSchema = new mongoose.Schema<UserInterface>({
+const UserSchema = new mongoose.Schema<IUserSchema>({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 })
 
-UserSchema.pre<UserInterface>("save", async function (next) {
+UserSchema.pre<IUserSchema>("save", async function (next) {
   if (this.isModified("password") || this.isNew) {
     const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
@@ -20,8 +36,8 @@ UserSchema.pre<UserInterface>("save", async function (next) {
   next()
 })
 
-UserSchema.methods.comparePassword = function (candidatePassword) {
+UserSchema.methods.comparePassword = function (candidatePassword: string) {
   return bcrypt.compare(candidatePassword, this.password)
 }
 
-export const User = mongoose.model<UserInterface>("User", UserSchema)
+export const User = mongoose.model<IUserSchema>("User", UserSchema)
